ef8f3d0924c48179c15aa1d17b3c2a8d3c7ce260
DiggidyDev <valentinewilson@gmail.com>
2024-01-05 18:48:37 +0000

    Introduced listFiles method for IFileStores (and reordered methods alphabetically)

--- src/interfaces/IFileStore.ts
+++ src/interfaces/IFileStore.ts
@@ -1,11 +1,13 @@
 export interface IFileStore {
   init?(): Promise<any>;
 
-  saveFile(filePath: string, fileContent: Buffer): Promise<string | null>;
-
-  getFile(filePath: string): Promise<Buffer | null>;
-
   deleteFile(filePath: string): Promise<void>;
 
   fileExists(filePath: string): Promise<boolean>;
+
+  getFile(filePath: string): Promise<Buffer | null>;
+
+  listFiles(recursive?: boolean): Promise<string[]>;
+
+  saveFile(filePath: string, fileContent: Buffer): Promise<string | null>;
 }
